7c61f83f01543156fcd2d1a635b022daa1c970a4
Julian Torres <julian.torres@mongodb.com>
2018-09-03 23:38:20 -0400

    trying out some travis CI stuff
    Signed-off-by: Julian Torres <julian.torres@mongodb.com>

--- install.sh
+++ install.sh
@@ -18,7 +18,13 @@ if ! sudo grep -q "%wheel		ALL=(ALL) NOPASSWD: ALL #atomantic/dotfiles" "/etc/su
 
   # Ask for the administrator password upfront
   bot "Next, I need you to enter your sudo password so I can install some things:"
-  sudo -v
+  if [[ $TESTING_INSTALL = 1]; then
+    # we're in travis, maybe?
+    bot "oh shit whaddup"
+    sudo -v
+  else
+    sudo -v
+  fi
 
   # Keep-alive: update existing sudo time stamp until the script has finished
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
