019598e52efc86935006fc6813bef3bed8445d3f
Julian Torres <julian.torres@mongodb.com>
2019-06-05 18:00:41 -0400

    -s is not working the way I want, but I'm giving up for now

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -56,52 +56,6 @@ ii() { #   ii:  display useful host related informaton
   echo
 }
 
-git-clean-local() {
-  # This function doesn't work, use at your own risk because it's dumb right now.
-  usage() {
-    cat <<EOF >&2
-This function doesn't work, use at your own risk because it's dumb right now.
-Usage: git-clean-local [-s] [-D] [-h]
-
--s : View (but don't delete) local branches that are not on remote
--D : Deletes all local branches that are not on remote
--h : Display this help text
-
-EOF
-  }
-
-  while getopts sDh o; do
-    case $o in
-    s)
-      gone_local=$(git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}')
-
-      if ! [ $gone_local ]; then
-        echo "No local branch has been deleted on remote." >&2
-        return 1
-      fi
-
-      if [ -z "$1" ]; then
-        echo "Fetching remotes..."
-        git fetch --all
-        echo "___________________________________________________________________________"
-        echo "The following local branches have been deleted on the remote repositories:"
-        echo $gone_local
-        echo "To delete them all, run the following command:"
-        echo "git-clean-local -D"
-        echo "---------------------------------------------------------------------------"
-      fi
-      ;;
-    D)
-      echo "Fetching remotes before deleting..."
-      git fetch --all
-      git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
-      ;;
-    h) usage ;;
-    *) usage ;;
-    esac
-  done
-}
-
 # }
 # }}}
 
