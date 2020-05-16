b503bc89dd3518a55163a97dfd0d0b4f77da5ec1
Julian Torres <julian.torres@mongodb.com>
2020-05-16 15:34:13 -0400

    new binding for ctrl+f

--- zsh/config/end/keybindings.zsh
+++ zsh/config/end/keybindings.zsh
@@ -12,3 +12,4 @@ bindkey '^e' edit-command-line # allows you to edit the current command line in 
 bindkey -s '^o' 'r\n'          # runs the 'r' command with 'ranger'
 bindkey '^T' fzf-completion
 bindkey '^R' fzf-history-widget
+bindkey -s '^F' 'zshz \t'
