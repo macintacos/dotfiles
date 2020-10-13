2cfa626ba87e3c1081c5aa2dd561104b1829886c
Julian Torres <julian.m.tor@gmail.com>
2020-10-13 14:17:53 -0400

    Update aliases and plugins

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -75,18 +75,18 @@ alias flatten='find . -mindepth 2 -type f -print0 | xargs -0 -I {} mv --backup=n
 # }}}
 
 # Making git a function to get around completion issues: https://github.com/tj/git-extras/issues/797
-git() {
-  # https://unix.stackexchange.com/a/257208
-  case "$PWD" in
-  "$HOME"/GitLocal/Website/* | "$HOME"/GitLocal/Learning/* | "$HOME"/GitLocal/Play/*)
-    hub "$@"
-    hub config user.email julian.m.tor@gmail.com
-    hub config user.signingkey "$PERSONAL_SECRET_GPG_KEY"
-    ;;
-  *)
-    hub "$@"
-    ;;
-  esac
-}
+# git() {
+#   # https://unix.stackexchange.com/a/257208
+#   case "$PWD" in
+#   "$HOME"/GitLocal/Website/* | "$HOME"/GitLocal/Learning/* | "$HOME"/GitLocal/Play/*)
+#     hub "$@"
+#     hub config user.email julian.m.tor@gmail.com
+#     hub config user.signingkey "$PERSONAL_SECRET_GPG_KEY"
+#     ;;
+#   *)
+#     hub "$@"
+#     ;;
+#   esac
+# }
 
 # compdef git="hub"
