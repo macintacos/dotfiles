1daecbdc26046612b6bb00f640d8810b26a3a0bf
Julian Torres <julian.m.tor@gmail.com>
2021-04-01 23:28:08 -0400

    Add pr-checkout function (relies on `glow` for preview, `gh` for PRs)

--- zsh/config/plugins/fzf.zsh
+++ zsh/config/plugins/fzf.zsh
@@ -27,7 +27,6 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS="
 --height 60%
 --reverse
---border
 --color=dark
 --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
 --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
