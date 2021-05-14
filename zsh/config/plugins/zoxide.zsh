128551b7d479159f69632e6d37a881393328515d
Julian Torres <julian.m.tor@gmail.com>
2021-05-13 20:22:42 -0400

    Color changes (went to Gruvbox and back again)

--- zsh/config/plugins/zoxide.zsh
+++ zsh/config/plugins/zoxide.zsh
@@ -3,13 +3,37 @@
 
 export _ZO_EXCLUDE_DIRS="$HOME/Library/Containers/*"
 
+# Dracula colors
+local background="#282a36"
+local current_line="#44475a"
+local selection="#44475a"
+local foreground="#f8f8f2"
+local comment="#6272a4"
+local cyan="#8be9fd"
+local green="#50fa7b"
+local orange="#ffb86c"
+local pink="#ff79c6"
+local purple="#bd93f9"
+local red="#ff5555"
+local yellow="#f1fa8c"
+
+# For Gruvbox
+# export _ZO_FZF_OPTS="
+#   --height 60%
+#   --reverse
+#   --color=light
+#   --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
+#   --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
+#   --bind tab:down
+#   --cycle
+# "
+# For Dracula
 export _ZO_FZF_OPTS="
   --height 60%
   --reverse
-  --border
   --color=dark
-  --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
-  --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
-  --bind tab:down
+  --color fg:-1,bg:$background,hl:$green,fg+:$foreground,bg+:$current_line,hl+:$pink
+  --color info:$comment,prompt:$green,spinner:$green,pointer:$cyan,marker:$pink,header:$purple
+  --bind=tab:down,btab:up
   --cycle
 "
