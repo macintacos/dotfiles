71d1824d7db1c466c73aff5a53c734abec6c06d4
Julian Torres <julian.torres@mongodb.com>
2022-03-09 23:38:21 -0500

    New backup

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -13,6 +13,7 @@ if status --is-interactive
         source $config
     end
 end
+
 set -x N_PREFIX "$HOME/.n"
 contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
