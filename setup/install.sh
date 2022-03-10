33854538b959d0cdfa4912a6fc41994902311004
Julian Torres <julian.torres@mongodb.com>
2022-03-09 23:39:02 -0500

    Adjustments to setup

--- setup/install.sh
+++ setup/install.sh
@@ -12,50 +12,60 @@ IFS=$'\n\t'
 trap "exit 1" SIGUSR1
 PROC=$$
 err_exit() {
-    echo -e "$@" >&2
-    kill -10 $PROC
+	echo -e "$@" >&2
+	kill -10 $PROC
 }
 
 setup_directory="${PWD##*/}"
 log info "You're currently here: $setup_directory"
 
 if [[ "${setup_directory}" != "dotfiles" ]]; then
-    err_exit "You need to run this from the dotfiles root with the 'just' program. Once 'just' is installed, run 'just setup'"
+	err_exit "You need to run this from the dotfiles root with the 'just' program. Once 'just' is installed, run 'just setup'"
 fi
 
+# Ask for the administrator password upfront
+log info "We need your password in order to setup things properly."
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
+done 2>/dev/null &
 
 ./setup/tools/node   # Setup node with 'n' and some global packages
-# ./setup/tools/python # Setup pyenv with some global requirements
-# ./setup/tools/asdf   # Setup asdf-managed CLI tools
-# ./setup/tools/brew   # Setup Homebrew - needs to be done first
-# ./setup/tools/java   # Setup Java-related things
-# ./setup/tools/neovim # Setup Neovim environment
+./setup/tools/python # Setup pyenv with some global requirements
+./setup/tools/kube   # Setup asdf-managed CLI tools
+./setup/tools/brew   # Setup Homebrew - needs to be done first
+./setup/tools/java   # Setup Java-related things
+./setup/tools/neovim # Setup Neovim environment
 ./setup/tools/mongo  # Setup things for MongoDB
 ./setup/tools/vscode # Setup VSCode - maybe not?
 
-./setup/symlink.sh # Symlink things
-./macos/_main      # Setting up macOS system settings...
+./setup/symlink.sh  # Symlink things
+./setup/macos/_main # Setting up macOS system settings...
 
 log info "Pulling down a bunch of repositories we care about..."
 (
-    cd "$HOME/GitLocal/Play"
-    gh repo clone macintacos/jmt.dev
-    gh repo clone macintacos/site
+	cd "$HOME/GitLocal/Play"
+	gh repo clone macintacos/jmt.dev
+	gh repo clone macintacos/site
 )
 (
-    cd "$HOME/GitLocal/Work"
-    gh repo clone mongodb/mongo
-    gh repo clone mongodb/mongocli
+	cd "$HOME/GitLocal/Work"
+	gh repo clone mongodb/mongo
+	gh repo clone mongodb/mongocli
 )
 (
-    cd "$HOME/GitLocal/Community"
-    gh repo clone python-poetry/poetry
-    gh repo clone LunarVim/LunarVim
-    gh repo clone VSpaceCode/VSpaceCode
+	cd "$HOME/GitLocal/Community"
+	gh repo clone python-poetry/poetry
+	gh repo clone LunarVim/LunarVim
+	gh repo clone VSpaceCode/VSpaceCode
 )
 (
-    cd "$HOME/GitLocal/Learning"
-    gh repo clone macintacos/advent-of-code
+	cd "$HOME/GitLocal/Learning"
+	gh repo clone macintacos/advent-of-code
 )
 
 log ok "Done!"
--- setup/symlink.sh
+++ setup/symlink.sh
@@ -4,95 +4,96 @@ export PATH="$HOME/GitLocal/Play/dotfiles/setup:$PATH"
 DOTFILES_HOME="$HOME/GitLocal/Play/dotfiles"
 
 bash_link() {
-    log info "Linking BASH..."
-    rm -rf ~/.bashrc
-    ln -sF "${DOTFILES_HOME}/rc/.bashrc" ~/.bashrc
-    log ok "BASH linked."
+	log info "Linking BASH..."
+	rm -rf ~/.bashrc
+	ln -sF "${DOTFILES_HOME}/rc/.bashrc" ~/.bashrc
+	log ok "BASH linked."
 }
 
 karabiner_link() {
-    log info "Linking Karabiner..."
-    rm -rf ~/.config/karabiner
-    mkdir -p ~/.config
-    ln -sF "${DOTFILES_HOME}/karabiner" ~/.config/karabiner
-    log ok "Karabiner linked."
+	log info "Linking Karabiner..."
+	rm -rf ~/.config/karabiner
+	mkdir -p ~/.config
+	ln -sF "${DOTFILES_HOME}/karabiner" ~/.config/karabiner
+	log ok "Karabiner linked."
 }
 
 nvim_link() {
-    log info "Linking neovim..."
-    rm -f ~/.config/nvim
-    ln -sF "${HOME}/GitLocal/Play/nvim" ~/.config/nvim
-    log ok "neovim linked."
+	log info "Linking neovim..."
+	rm -f ~/.config/nvim
+	ln -sF "${HOME}/GitLocal/Play/nvim" ~/.config/nvim
+	log ok "neovim linked."
 }
 
 lnav_link() {
-    log info "Linking lnav..."
-    rm -rf "$HOME/.lnav/formats/installed"
-    mkdir -p "$HOME/.lnav/formats/installed"
-    ln -sF "${DOTFILES_HOME}/lnav/formats/installed" "$HOME/.lnav/formats/installed"
-    log ok "lnav formats linked."
+	log info "Linking lnav..."
+	rm -rf "$HOME/.lnav/formats/installed"
+	mkdir -p "$HOME/.lnav/formats/installed"
+	ln -sF "${DOTFILES_HOME}/lnav/formats/installed" "$HOME/.lnav/formats/installed"
+	log ok "lnav formats linked."
 }
 
 gitconfig_link() {
-    log info "Linking gitconfig..."
-    rm -rf "$HOME/.gitconfig"
-    ln -sF "${DOTFILES_HOME}/.gitconfig" "$HOME/.gitconfig"
-    log ok ".gitconfig linked."
+	log info "Linking gitconfig..."
+	rm -rf "$HOME/.gitconfig"
+	ln -sF "${DOTFILES_HOME}/.gitconfig" "$HOME/.gitconfig"
+	log ok ".gitconfig linked."
 }
 
 gitignore_global_link() {
-    log info "Linking .gitignore global config..."
-    rm -rf "$HOME/.gitignore_global"
-    ln -sF "${DOTFILES_HOME}/.gitignore_global" "$HOME/.gitignore_global"
-    log ok ".gitignore_global linked."
+	log info "Linking .gitignore global config..."
+	rm -rf "$HOME/.gitignore_global"
+	ln -sF "${DOTFILES_HOME}/.gitignore_global" "$HOME/.gitignore_global"
+	log ok ".gitignore_global linked."
 }
 
 fish_link() {
-    log info "Linking fish stuff..."
-    rm -rf "$HOME/.config/fish"
-    ln -sF "${DOTFILES_HOME}/shell/fish" "$HOME/.config/fish"
-    log ok "fish linked."
+	log info "Linking fish stuff..."
+	rm -rf "$HOME/.config/fish"
+	ln -sF "${DOTFILES_HOME}/shell/fish" "$HOME/.config/fish"
+	log ok "fish linked."
 }
 
 ideavimrc_link() {
-    log info "Linking .ideavimrc..."
-    rm -f "$HOME/.ideavimrc"
-    ln -sF "${DOTFILES_HOME}/rc/.ideavimrc" "$HOME/.ideavimrc"
-    log ok ".ideavimrc linked."
+	log info "Linking .ideavimrc..."
+	rm -f "$HOME/.ideavimrc"
+	ln -sF "${DOTFILES_HOME}/rc/.ideavimrc" "$HOME/.ideavimrc"
+	log ok ".ideavimrc linked."
 }
 
 zsh_link() {
-    log info "Linking ZSH..."
-    rm -rf ~/.zshrc
-    rm -rf ~/.zshenv
-    rm -rf ~/.zsh_history
-    ln -sF "${DOTFILES_HOME}/shell/zsh/.zshenv" ~/.zshenv
-    ln -sF "${DOTFILES_HOME}/shell/zsh/.zshrc" ~/.zshrc
-    ln -sF "${DOTFILES_HOME}/shell/zsh/.zsh_history" ~/.zsh_history
-    log ok "ZSH linked"
+	log info "Linking ZSH..."
+	rm -rf ~/.zshrc
+	rm -rf ~/.zshenv
+	rm -rf ~/.zsh_history
+	ln -sF "${DOTFILES_HOME}/shell/zsh/.zshenv" ~/.zshenv
+	ln -sF "${DOTFILES_HOME}/shell/zsh/.zshrc" ~/.zshrc
+	ln -sF "${DOTFILES_HOME}/shell/zsh/.zsh_history" ~/.zsh_history
+	log ok "ZSH linked"
 }
 
 lazygit_link() {
-    log info "Linking lazygit config..."
-    rm -rf "${HOME}/Library/Application Support/lazygit/config.yml"
-    ln -sF "${DOTFILES_HOME}/rc/.lazygitrc" "${HOME}/Library/Application Support/lazygit/config.yml"
-    log ok "lazygit linked."
+	log info "Linking lazygit config..."
+	rm -rf "${HOME}/Library/Application Support/lazygit/config.yml"
+	mkdir -p "${HOME}/Library/Application Support/lazygit"
+	ln -sF "${DOTFILES_HOME}/rc/.lazygitrc" "${HOME}/Library/Application Support/lazygit/config.yml"
+	log ok "lazygit linked."
 }
 
 ## ALL {{{
 link_all() {
-    log info "Creating all symlinks..."
-    bash_link
-    gitignore_global_link
-    gitconfig_link
-    ideavimrc_link
-    karabiner_link
-    lazygit_link
-    lnav_link
-    fish_link
-    nvim_link
-    zsh_link
-    log ok "All symlinks created."
+	log info "Creating all symlinks..."
+	bash_link
+	gitignore_global_link
+	gitconfig_link
+	ideavimrc_link
+	karabiner_link
+	lazygit_link
+	lnav_link
+	fish_link
+	nvim_link
+	zsh_link
+	log ok "All symlinks created."
 }
 
 link_all
