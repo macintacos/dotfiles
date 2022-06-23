5c83c82a42998ad409ddfb3b2bc85c4427b96e06
Julian Torres <julian.torres@mongodb.com>
2022-06-23 14:07:26 -0400

    Update fish config such that git-branchless is used more readily

--- shell/fish/user/functions/git.fish
+++ shell/fish/user/functions/git.fish
@@ -2,3 +2,12 @@
 function cdg --description "'cd' to the top level of a git project"
     cd (git --rev-parse --show-top-level)
 end
+
+function gst --description "Start Lazygit (and conditionally init branchless)"
+    if not git sl &>/dev/null
+        echo "Initializing git branchless before starting lazygit..."
+        git branchless init
+    end
+
+    lazygit
+end
