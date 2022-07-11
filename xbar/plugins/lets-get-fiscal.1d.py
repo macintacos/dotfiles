588a0865222ef63661d9b0e52cb99fd118d2a6b1
Julian Torres <julian.torres@mongodb.com>
2022-07-10 21:57:04 -0400

    1.0 of 'lets-get-fiscal'

--- xbar/plugins/lets-get-fiscal.1d.py
+++ xbar/plugins/lets-get-fiscal.1d.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-# NOTE: This gets run with the Python from XCode, NOT with pyenv. I don't believe
-#       there's a way to change this. As such, this plugin will install a module
-#       automatically to compensate for having an unreliable environment.
-
 # <xbar.title>Fiscal Year Generator</xbar.title>
-# <xbar.desc>Gets the current MongoDB fiscal year and presents it to the user.</xbar.desc>
-# <xbar.version>v0.1</xbar.version>
+# <xbar.desc>Gets the current fiscal year/quarter and presents it to the user, as well as other goodies.</xbar.desc>
+# <xbar.version>v1.0</xbar.version>
 # <xbar.author>Julian Torres</xbar.author>
 # <xbar.author.github>macintacos</xbar.author.github>
 # <xbar.dependencies>python</xbar.dependencies>
@@ -14,6 +10,10 @@
 
 # PREFERENCES
 # <xbar.var>string(VAR_CURRENT_PLAN_LINK="https://docs.google.com/spreadsheets/u/0/?tgif=c&q=Cloud%20Team%20Quarterly%20Plan"): Link to the current quarterly plan.</xbar.var>
+# <xbar.var>string(VAR_Q1_MONTHS="2,3,4"): Months that are in Q1 (valid values are numbers, comma-separated, where January = 1 and December = 12)</xbar.var>
+# <xbar.var>string(VAR_Q2_MONTHS="5,6,7"): Months that are in Q2 (valid values are numbers, comma-separated, where January = 1 and December = 12)</xbar.var>
+# <xbar.var>string(VAR_Q3_MONTHS="8,9,10"): Months that are in Q3 (valid values are numbers, comma-separated, where January = 1 and December = 12)</xbar.var>
+# <xbar.var>string(VAR_Q4_MONTHS="11,12,1"): Months that are in Q4 (valid values are numbers, comma-separated, where January = 1 and December = 12)</xbar.var>
 
 from __future__ import annotations
 
@@ -39,23 +39,43 @@ for attempt in range(3):
 
 ValidQuarters = Optional[Literal["Q1", "Q2", "Q3", "Q4"]]
 
+# Environment Variables from xBar (defined above)
+# fmt: off
+Q1_MONTHS: list[int] = [int(q.strip()) for q in os.getenv("VAR_Q1_MONTHS", "2,3,4").split(",")]
+Q2_MONTHS: list[int] = [int(q.strip()) for q in os.getenv("VAR_Q2_MONTHS", "5,6,7").split(",")]
+Q3_MONTHS: list[int] = [int(q.strip()) for q in os.getenv("VAR_Q3_MONTHS", "8,9,10").split(",")]
+Q4_MONTHS: list[int] = [int(q.strip()) for q in os.getenv("VAR_Q4_MONTHS", "11,12,1").split(",")]
+# fmt: on
+
+# If there aren't 12 entries _or_ there are duplicates, reject early.
+if (
+    len(Q1_MONTHS + Q2_MONTHS + Q3_MONTHS + Q4_MONTHS)
+    != len(set(Q1_MONTHS + Q2_MONTHS + Q3_MONTHS + Q4_MONTHS))
+    or len(Q1_MONTHS + Q2_MONTHS + Q3_MONTHS + Q4_MONTHS) != 12
+):
+    raise Exception(
+        "Issue initializing fiscal plugin. Review quarterly month preferences."
+    )
+
 
 def get_fiscal_quarter(target: date) -> ValidQuarters:
+    """Gets the fiscal quester for the given date."""
     current_month: int = target.month
 
-    if current_month in [2, 3, 4]:
+    if current_month in Q1_MONTHS:
         return "Q1"
-    elif current_month in [5, 6, 7]:
+    elif current_month in Q2_MONTHS:
         return "Q2"
-    elif current_month in [8, 9, 10]:
+    elif current_month in Q3_MONTHS:
         return "Q3"
-    elif current_month in [11, 12, 1]:
+    elif current_month in Q4_MONTHS:
         return "Q4"
     else:
         return
 
 
 def get_fiscal_year(target: date) -> str:
+    """Gets the fiscal year for the given date."""
     current_year: int = target.year
 
     fiscal_quarter: ValidQuarters
@@ -75,17 +95,28 @@ def get_fiscal_year(target: date) -> str:
 
 
 def get_fiscal_year_and_quarter(target: date) -> str:
+    """Generates the fiscal year string, in the form of 'FY<year>Q<quarter>'."""
     return f"{get_fiscal_year(target)}{get_fiscal_quarter(target)}"
 
 
-def get_date_range_for_fiscal_quarter(fiscal_year_quarter: str) -> Optional[str]:
+def get_date_range_for_fiscal_quarter(
+    fiscal_year_quarter: str,
+) -> tuple[str, str]:
+    """Gets the date range as a tuple for the given fiscal year string.
+
+    Args:
+        fiscal_year_quarter (str): An FY string, i.e. 'FY2022Q3'
+
+    Returns:
+        tuple[str, str]: A tuple containing the beginning end end dates for the given FY
+            string passed in.
+    """
     # Some light validation for the string passed in.
     if not fiscal_year_quarter.startswith("FY"):
-        return None
+        raise Exception("String provided did not start with 'FY', somehow.")
 
     # fmt: off
-    (fy_string, year, quarter) = (
-        fiscal_year_quarter[:2],       # FY
+    (year, quarter) = (
         int(fiscal_year_quarter[2:6]),  # 20XX
         fiscal_year_quarter[6:],      # QX
     )
@@ -102,11 +133,11 @@ def get_date_range_for_fiscal_quarter(fiscal_year_quarter: str) -> Optional[str]
     elif quarter == "Q2":
         begin = f"{year - 1}-05-01"
         end = f"{year - 1}-07-31"
-    else:
+    else:  # Q1
         begin = f"{year - 1}-02-01"
         end = f"{year - 1}-04-31"
 
-    return f"{begin} to {end}"
+    return (begin, end)
 
 
 def get_next_quarters(num_quarters: int) -> list[str]:
@@ -126,9 +157,31 @@ def get_next_quarters(num_quarters: int) -> list[str]:
     return fiscal_list
 
 
+def get_weeks_to_next_quarter(quarter_beginning_iso_string: str) -> int:
+    """For the given ISO string, it tells you how many weeks away you are from that quarter beginning."""
+    today: date = date.today()
+    (year, month, day) = quarter_beginning_iso_string.split("-")
+    next_quarter_beginning = date(int(year), int(month), int(day))
+
+    return (next_quarter_beginning - today).days // 7
+
+
 def main():
     next_five_quarters: list[str] = get_next_quarters(5)
     q1, q2, q3, q4, q5 = next_five_quarters
+
+    # Get the quarter range messages
+    q2_msg: str = " to ".join(get_date_range_for_fiscal_quarter(q2))
+    q3_msg: str = " to ".join(get_date_range_for_fiscal_quarter(q3))
+    q4_msg: str = " to ".join(get_date_range_for_fiscal_quarter(q4))
+    q5_msg: str = " to ".join(get_date_range_for_fiscal_quarter(q5))
+
+    # Getting the weeks until the next quarter for presentation later.
+    next_quarter_begin, _ = get_date_range_for_fiscal_quarter(q2)
+    weeks_until_quarter: int = get_weeks_to_next_quarter(next_quarter_begin)
+    weeks_until_quarter_msg: str = (
+        f"In {weeks_until_quarter} weeks" if weeks_until_quarter != 0 else "Next week!"
+    )
 
     # Generate the final menu
     print(
@@ -136,14 +189,14 @@ def main():
             f"""
             {q1} | font=MonoLisa-Regular
             ---
-            :link: Current Plan ({q1}) | font=MonoLisa-Regular | href={os.getenv("VAR_CURRENT_PLAN_LINK")}
+            :link: Current Plan ({q1}) | size=14 | font=MonoLisa-Regular | href={os.getenv("VAR_CURRENT_PLAN_LINK")}
             ---
-            Next 4 FYQs | font=MonoLisa-Bold | size=16
+            Next 4 Fiscal Quarters | font=MonoLisa-Bold | size=16 | href=https://jira.mongodb.org/issues/?jql=labels%20in%20({q2},{q3},{q4},{q5})%20ORDER%20BY%20updated%20DESC
             ---
-            {q2} -> ({get_date_range_for_fiscal_quarter(q2)}) | font=MonoLisa-Regular | size=14 | color=red
-            {q3} -> ({get_date_range_for_fiscal_quarter(q3)}) | font=MonoLisa-Regular | size=14 | color=yellow
-            {q4} -> ({get_date_range_for_fiscal_quarter(q4)}) | font=MonoLisa-Regular | size=14 | color=green
-            {q5} -> ({get_date_range_for_fiscal_quarter(q5)}) | font=MonoLisa-Regular | size=14 | color=green
+            :link: {q2} -> ({q2_msg}) [{weeks_until_quarter_msg}] | font=MonoLisa-Regular | color={"red" if weeks_until_quarter <= 6 else "green"} | href=https://jira.mongodb.org/issues/?jql=labels%20in%20({q2})%20ORDER%20BY%20updated%20DESC
+            :link: {q3} -> ({q3_msg}) [In {weeks_until_quarter + 12} weeks] | font=MonoLisa-Regular | href=https://jira.mongodb.org/issues/?jql=labels%20in%20({q3})%20ORDER%20BY%20updated%20DESC
+            :link: {q4} -> ({q4_msg}) [In {weeks_until_quarter + 24} weeks] | font=MonoLisa-Regular | href=https://jira.mongodb.org/issues/?jql=labels%20in%20({q4})%20ORDER%20BY%20updated%20DESC
+            :link: {q5} -> ({q5_msg}) [In {weeks_until_quarter + 36} weeks] | font=MonoLisa-Regular | href=https://jira.mongodb.org/issues/?jql=labels%20in%20({q5})%20ORDER%20BY%20updated%20DESC
             """
         )
     )
