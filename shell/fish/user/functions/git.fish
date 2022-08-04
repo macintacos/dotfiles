e7a860d0591f5b4b021e26fbb8ec2b86e3c22151
Julian Torres <julian.torres@mongodb.com>
2022-08-03 20:49:50 -0400

    Adjust git function

--- shell/fish/user/functions/git.fish
+++ shell/fish/user/functions/git.fish
@@ -1,6 +1,6 @@
 # git-related functions
 function cdg --description "'cd' to the top level of a git project"
-    cd (git --rev-parse --show-top-level)
+    cd (git rev-parse --show-toplevel)
 end
 
 function gst --description "Start Lazygit (and conditionally init branchless)"
