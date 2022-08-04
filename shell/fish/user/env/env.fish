ab90d290db7edef35df4949f53581977d8d6631c
Julian Torres <julian.torres@mongodb.com>
2022-08-03 20:33:04 -0400

    Update KUBECONFIG

--- shell/fish/user/env/env.fish
+++ shell/fish/user/env/env.fish
@@ -18,7 +18,6 @@ set -gx CHEAT_COLORS true
 set -gx CHEAT_COLORSCHEME dark
 set -gx COLORTERM truecolor
 set -gx GOPATH $HOME/GoWorkspace
-# set -gx GOROOT $(brew --prefix)/bin/go
 set -gx HOME_LOCAL_BIN $HOME/.local/bin
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p --style=numbers,grid'"
 set -gx MMS_HOME $GITLOCAL_HOME/Work/mms
@@ -29,7 +28,7 @@ set -gx RUST $HOME/.cargo/bin
 set -gx TERM xterm-256color
 set -gx PLUG_NVIM_HOME $NVIM_HOME/viml/plug
 set -gx VASA_HOME $HOME/.vasa
-set -gx KUBECONFIG "$HOME/.kube/config.prod:$HOME/.kube/config.staging"
+set -gx KUBECONFIG "$HOME/.kube/config"
 
 # fisher
 set -gx fisher_path $FISH_HOME
@@ -48,9 +47,10 @@ fish_add_path -a (brew --prefix)/opt/gnu-sed/libexec/gnubin
 fish_add_path -a (brew --prefix)/opt/coreutils/libexec/gnubin
 fish_add_path -a (brew --prefix)/opt/grep/libexec/gnubin
 fish_add_path -a (brew --prefix)/opt/make/libexec/gnubin
+fish_add_path -a (brew --prefix)/opt/findutils/libexec/gnubin
 fish_add_path -a (brew --prefix)/opt/openssl@3/bin
 
 # Custom scripts
-fish_add_path -a $DOTFILES_HOME/shell/scripts/python
+fish_add_path -a $DOTFILES_HOME/shell/tools/python
 fish_add_path -a $GITLOCAL_HOME/Work/ops-tools/scripts/python/sshnds/bin
 fish_add_path -a $HOME/MongoDB/coe-tooling
