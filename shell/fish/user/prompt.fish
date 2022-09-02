8db7578998433c5121bae2416e1370498b13d10b
Julian Torres <julian.torres@mongodb.com>
2022-09-02 11:01:22 -0400

    fix(fish): adjust vi-mode instantiation, and fix vi-mode cursor changing

--- shell/fish/user/prompt.fish
+++ shell/fish/user/prompt.fish
@@ -5,30 +5,6 @@ function fish_greeting
 end
 
 # Prompt
-fish_vi_key_bindings
-
-function fish_mode_prompt
-    if test "$fish_key_bindings" != fish_default_key_bindings
-        set --local vi_mode_color
-        set --local vi_mode_symbol
-        switch $fish_bind_mode
-            case default
-                set vi_mode_color (set_color --reverse magenta)
-                set vi_mode_symbol N
-            case insert
-                set vi_mode_color (set_color --reverse green)
-                set vi_mode_symbol I
-            case replace replace_one
-                set vi_mode_color (set_color --reverse red)
-                set vi_mode_symbol R
-            case visual
-                set vi_mode_color (set_color --reverse cyan)
-                set vi_mode_symbol V
-        end
-        string unescape "$vi_mode_color $vi_mode_symbol \x1b[0m "
-    end
-end
-
 set hydro_color_pwd green -d
 set hydro_color_git brblack
 set hydro_color_prompt brblack
