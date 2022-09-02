f727d38e0557d2333eac37bd2fca59e702c22b21
Julian Torres <julian.torres@mongodb.com>
2022-09-02 11:01:22 -0400

    fix(fish): adjust vi-mode instantiation, and fix vi-mode cursor changing

--- shell/fish/user/keybinds.fish
+++ shell/fish/user/keybinds.fish
@@ -4,9 +4,14 @@
 bind \cf "fzf | xargs code; commandline -f repaint"
 bind -M insert \cf "fzf | xargs code; commandline -f repaint"
 
-# Ctrl+d to navigate to a directory using zoxide
+# Ctrl+g to navigate to a common directory using zoxide
 bind \cg "__zoxide_zi; commandline -f repaint"
 bind -M insert \cg "__zoxide_zi; commandline -f repaint"
 
-# Remap tab to shift+tab when in insert mode
-bind -M insert -k nul complete-and-search
+# Unbind tab/btab, since we are trying out Fig
+bind \t true
+bind -M insert \t true
+bind -M visual \t true
+bind -k btab true
+bind -M insert -k btab true
+bind -M visual -k btab true
--- shell/fish/user/vi.fish
+++ shell/fish/user/vi.fish
@@ -1,14 +1,38 @@
-# Emulates vim's cursor shape behavior
-
-# Set the normal and visual mode cursors to a block
 set fish_cursor_default block
-
-# Set the insert mode cursor to a line
+set fish_cursor_visual block
 set fish_cursor_insert line
-
-# Set the replace mode cursor to an underscore
 set fish_cursor_replace_one underscore
 
-# The following variable can be used to configure cursor shape in
-# visual mode, but due to fish_cursor_default, is redundant here
-set fish_cursor_visual block
+fish_vi_key_bindings
+fish_vi_cursor
+
+# Change the way mode looks in the prompt
+# FIXME: When this issue is resolved, see if you can remove the `__fish_cursor_xterm` hacks.
+#        https://github.com/fish-shell/fish-shell/issues/9172
+#
+#        Workaround was found in this issue: https://github.com/fish-shell/fish-shell/issues/8981#issuecomment-1191576764
+function fish_mode_prompt
+    if test "$fish_key_bindings" != fish_default_key_bindings
+        set --local vi_mode_color
+        set --local vi_mode_symbol
+        switch $fish_bind_mode
+            case default
+                set vi_mode_color (set_color --reverse magenta)
+                set vi_mode_symbol N
+                __fish_cursor_xterm block # WORKAROUND
+            case insert
+                set vi_mode_color (set_color --reverse green)
+                set vi_mode_symbol I
+                __fish_cursor_xterm line # WORKAROUND
+            case replace replace_one
+                set vi_mode_color (set_color --reverse red)
+                set vi_mode_symbol R
+                __fish_cursor_xterm underscore # WORKAROUND
+            case visual
+                set vi_mode_color (set_color --reverse cyan)
+                set vi_mode_symbol V
+                __fish_cursor_xterm block # WORKAROUND
+        end
+        string unescape "$vi_mode_color $vi_mode_symbol \x1b[0m "
+    end
+end
