9c231275edf17a5ae55fbbdd658058b29c7c76b0
Julian Torres <julian.torres@mongodb.com>
2023-01-15 21:40:57 -0500

    fix(fish): `jira-open` points to the right address

--- shell/fish/user/functions/jira.fish
+++ shell/fish/user/functions/jira.fish
@@ -1,3 +1,3 @@
 function jira-open --description "Open Jira, using the current branch name as a parameter for the link to open"
-    open "https://jira.mongodb.com/browse/"(git branch --show-current)
+    open "https://jira.mongodb.org/browse/"(git branch --show-current)
 end
