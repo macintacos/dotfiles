f5fff0d0a72c271bb0d8d30cb9e04b052b2b6839
Julian Torres <julian.torres@mongodb.com>
2023-01-15 21:39:22 -0500

    chore: fix formatting

--- shell/fish/user/keybinds.fish
+++ shell/fish/user/keybinds.fish
@@ -7,4 +7,3 @@ bind -M insert \cf "fzf | xargs code; commandline -f repaint"
 # Ctrl+g to navigate to a common directory using zoxide
 bind \cg "__zoxide_zi; commandline -f repaint"
 bind -M insert \cg "__zoxide_zi; commandline -f repaint"
-
