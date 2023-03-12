331dec5c3219ec571672cfb6be9167842c87fb24
Julian Torres <julian.torres@mongodb.com>
2023-03-12 17:47:52 -0400

    feat: add new `klog` function to ease tailing k8s pod logs

--- shell/fish/user/functions/kube.fish
+++ shell/fish/user/functions/kube.fish
@@ -11,3 +11,17 @@ function kget
         jq -r ".items | map(select(.metadata.name | test(\"$glob\"))) | {kind: \"List\", items: .}" | \
         kubectl get -f -
 end
+
+function klog
+    if test (count $argv) -ne 1
+        echo "USAGE: klog \"<glob>\""
+        echo ""
+        echo "NOTE: this assumed that the first pod returned is the one you want to tail the logs of. If you're tailing the wrong logs, make the glob more targeted."
+        return 1
+    end
+
+    set -l glob "$argv[1]"
+    set -l pod $(kubectl get pods -o json | jq -r ".items | map(select(.metadata.name | test(\"$glob\")))[0].metadata.name")
+
+    kubectl logs -f "$pod"
+end
