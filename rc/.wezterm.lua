14967ec5634a4c90efd1b7fdf9f3a296944f8ce8
Julian Torres <julian.torres@mongodb.com>
2023-04-26 15:05:04 -0400

    feat: add wezterm config

---
+++ rc/.wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.color_scheme = 'nightfox'
+
+config.font = wezterm.font("Berkeley Mono")
+config.font_size = 15
+
+config.window_decorations = "NONE"
+
+return config
