378db89090de3e2f33d3e408a8d83f98fea27f98
Julian Torres <julian.torres@mongodb.com>
2024-03-10 18:09:29 -0400

    fix: get fzf working properly again

--- shell/fish/user/fifc.fish
+++ shell/fish/user/fifc.fish
@@ -1,2 +1,6 @@
 # For the gazorby/fifc plugin
-set -U fifc_exa_opts "--icons"
+set -U fifc_exa_opts --icons --tree
+set -Ux fifc_editor nvim
+set -Ux fifc_keybinding \t
+set -U fifc_bat_opts --style=numbers
+set -U fifc_fd_opts --hidden
--- shell/fish/user/fzf.fish
+++ shell/fish/user/fzf.fish
@@ -1,3 +1,14 @@
+##### OPTIONS #####
+
+set fzf_preview_dir_cmd exa --all --color=always
+fzf_configure_bindings \
+    --directory=\cf \
+    --processes=\cp \
+    --history=\cr \
+    --git_status=\ct \
+    --git_log=\cl
+##### COLORS #####
+
 # Dracula colors
 # set --local background "#0b0d0f"
 # set --local current_line "#131a20"
@@ -14,70 +25,41 @@
 # set --local yellow "#f1fa8c"
 
 # Nightfox colors
-set --local background "#192330"
-set --local current_line "#2b3b51"
-set --local selection "#2b3b51"
-set --local foreground "#cdcecf"
-set --local foreground "#cdcecf"
-set --local comment "#738091"
-set --local cyan "#63cdcf"
-set --local green "#81b29a"
-set --local orange "#f4a261"
-set --local pink "#d67ad2"
-set --local purple "#9d79d6"
-set --local red "#c94f6d"
-set --local yellow "#dbc074"
+# set --local background "#192330"
+# set --local current_line "#2b3b51"
+# set --local selection "#2b3b51"
+# set --local foreground "#cdcecf"
+# set --local foreground "#cdcecf"
+# set --local comment "#738091"
+# set --local cyan "#63cdcf"
+# set --local green "#81b29a"
+# set --local orange "#f4a261"
+# set --local pink "#d67ad2"
+# set --local purple "#9d79d6"
+# set --local red "#c94f6d"
+# set --local yellow "#dbc074"
 
 # FZF Default Settings
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
-set -gx FZF_DEFAULT_OPTS "\
-    --height 60%
-    --reverse
-    --color=dark
-    --color fg:-1,bg:$background,hl:$green,fg+:$foreground,bg+:$current_line,hl+:$pink
-    --color info:$comment,prompt:$green,spinner:$green,pointer:$cyan,marker:$pink,header:$purple
-    --bind=tab:down,btab:up
-    --bind='alt-k:preview-up,alt-p:preview-up'
-    --bind='alt-j:preview-down,alt-n:preview-down'
-    --bind='ctrl-r:toggle-all'
-    --bind='ctrl-s:toggle-sort'
-    --bind='?:toggle-preview'
-    --bind='alt-w:toggle-preview-wrap'
-    --cycle
-    --preview-window='right:60%'
-    --ansi
-    "
-
-# `forgit` settings
-set -x FORGIT_FZF_DEFAULT_OPTS "
-    --ansi
-    --height 60%
-    --bind=tab:down,btab:up
-    --bind='alt-k:preview-up,alt-p:preview-up'
-    --bind='alt-j:preview-down,alt-n:preview-down'
-    --bind='ctrl-r:toggle-all'
-    --bind='ctrl-s:toggle-sort'
-    --bind='?:toggle-preview'
-    --bind='alt-w:toggle-preview-wrap'
-    --preview-window='right:60%'
-    +1
-"
 
 # Zoxide FZF settings
 set -gx _ZO_FZF_OPTS "\
-    --height 60%
-    --reverse
+    --no-sort
+    --keep-right
+    --height=60%
+    --info=inline
+    --layout=reverse
+    --exit-0
+    --select-1
+    --bind=ctrl-z:ignore
+    --preview='exa {2..} --icons -a --group-directories-first --git -F'
+    --preview-window='right:60%,<100(down,30%)'
     --color=dark
-    --color fg:-1,bg:$background,hl:$green,fg+:$foreground,bg+:$current_line,hl+:$pink
-    --color info:$comment,prompt:$green,spinner:$green,pointer:$cyan,marker:$pink,header:$purple
     --bind=tab:down,btab:up
     --bind='alt-k:preview-up,alt-p:preview-up'
     --bind='alt-j:preview-down,alt-n:preview-down'
-    --bind='ctrl-r:toggle-all'
-    --bind='ctrl-s:toggle-sort'
-    --bind='?:toggle-preview'
     --bind='alt-w:toggle-preview-wrap'
-    --preview-window='right:60%'
+    --bind='?:toggle-preview'
     --cycle
     --ansi
     "
